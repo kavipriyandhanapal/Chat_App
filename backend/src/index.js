f7cc3893f1bdaade9760333d12deae784f1c398c
kavipriyan <kavipriyan@nobel-systems.com>
2025-05-25 16:38:21 +0530

    minor changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,10 +24,13 @@ app.use("/api/messages", messageRouter);
 
 const __dirname = path.resolve();
 
+console.log("testing");
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
